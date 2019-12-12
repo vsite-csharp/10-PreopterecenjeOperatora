90e852260021f0536ab6be1fc3b8549e8d76bf47
Julijan Šribar <julijan.sribar@gmail.com>
2019-12-12 01:03:30 +0100

    Initial commit

---
+++ OperatorImplicitnePretvorbe/OperatorImplicitnePretvorbe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Vsite.CSharp.PreopterećenjeOperatora
+{
+    // TODO:030 U strukturu Racionalni iz prethodnog projekta dodati operator implicitne konverzije iz long u Racionalni.
+
+    // TODO:032 U strukturu Racionalni iz prethodnog projekta dodati operator implicitne konverzije iz double u Racionalni.
+
+    class OperatorImplicitnePretvorbe
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Racionalni rac = new Racionalni();
+                // TODO:031 Otkomentirati donje naredbe i provjeriti ispis.
+                // koristi implicitnu pretvorbu iz int
+                //rac = 2;
+                //Debug.Assert(rac.ToString() == "2 / 1");
+                Console.WriteLine(rac);
+
+                // TODO:033 Otkomentirati donje naredbe i provjeriti ispis.
+                // koristi implicitnu pretvorbu iz double
+                //rac = 0.1;
+                //Debug.Assert(rac.ToString() == "1 / 10");
+                Console.WriteLine(rac);
+
+                //TODO:034 Pokrenuti i provjeriti testove (2 testa iz grupe "TestImplicitnePretvorbe" moraju proći).
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("GOTOVO!!!");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Testovi/TestImplicitnePretvorbe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.CSharp.PreopterećenjeOperatora.Testovi
+{
+    [TestClass]
+    public class TestImplicitnePretvorbe
+    {
+        [TestMethod]
+        public void Racionalni_ImplicitnaPretvorbaIzJedneDesetineDajeBrojSBrojnikom1Nazivnikom10()
+        {
+            var staticMethods = typeof(Racionalni).GetMethods(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            var opImplicitFromDouble = staticMethods.FirstOrDefault(mi => mi.Name == "op_Implicit" && mi.ReturnType == typeof(Racionalni) && mi.GetParameters().Count() == 1 && mi.GetParameters().ElementAt(0).ParameterType == typeof(double));
+            Assert.IsNotNull(opImplicitFromDouble);
+            Racionalni rac = (Racionalni)opImplicitFromDouble.Invoke(null, new object[] { 0.1 });
+            Assert.AreEqual(1, rac.Brojnik);
+            Assert.AreEqual(10, rac.Nazivnik);
+        }
+
+        [TestMethod]
+        public void Racionalni_ImplicitnaPretvorbaIzDvaDajeBrojSBrojnikom2Nazivnikom1()
+        {
+            var staticMethods = typeof(Racionalni).GetMethods(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            var opImplicitFromLong = staticMethods.FirstOrDefault(mi => mi.Name == "op_Implicit" && mi.ReturnType == typeof(Racionalni) && mi.GetParameters().Count() == 1 && mi.GetParameters().ElementAt(0).ParameterType == typeof(long));
+            Assert.IsNotNull(opImplicitFromLong);
+            Racionalni rac = (Racionalni)opImplicitFromLong.Invoke(null, new object[] { 2 });
+            Assert.AreEqual(2, rac.Brojnik);
+            Assert.AreEqual(1, rac.Nazivnik);
+        }
+    }
+}
