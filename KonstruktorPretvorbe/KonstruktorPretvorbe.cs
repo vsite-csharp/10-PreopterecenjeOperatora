342b0c938af263a39aa1e7b5423efb08aa18ae24
Marko Vitković <mavitkov@vsite.hr>
2019-12-12 21:15:23 +0100

    zaborvio napravit branch pa svi commiti od jednom, 01,02,03

--- KonstruktorPretvorbe/KonstruktorPretvorbe.cs
+++ KonstruktorPretvorbe/KonstruktorPretvorbe.cs
@@ -9,23 +9,19 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
         {
             try
             {
-                // TODO:020 Provjeriti konstruktor koji će donja naredba pozvati, pokrenuti program i provjeriti ispis.
                 Racionalni rac = new Racionalni(25);
                 Debug.Assert(rac.ToString() == "25 / 1");
                 Console.WriteLine(rac);
 
-                // TODO:022 Provjeriti koji konstruktor će donja naredba pozvati, otkomentirati naredbu te pokrenuti program i provjeriti ispis.
-                //rac = new Racionalni(0.25);
-                //Debug.Assert(rac.ToString() == "1 / 4");
-                Console.WriteLine(rac);
+				rac = new Racionalni(0.25);
+				Debug.Assert(rac.ToString() == "1 / 4");
+				Console.WriteLine(rac);
 
-                // TODO:023 Provjeriti koji konstruktor će donja naredba pozvati, otkomentirati naredbu te pokrenuti program i provjeriti ispis.
-                // Naredba koja poziva konstruktor pretvorbe iz double
-                //rac = new Racionalni(0.125F);
-                //Debug.Assert(rac.ToString() == "1 / 8");
-                Console.WriteLine(rac);
+				// Naredba koja poziva konstruktor pretvorbe iz double
+				rac = new Racionalni(0.125F);
+				Debug.Assert(rac.ToString() == "1 / 8");
+				Console.WriteLine(rac);
 
-                // TODO:024 Pokrenuti i provjeriti testove (3 testa iz grupe "TestKonstruktoraPretvorbe" moraju proći).
             }
             catch (System.Exception e)
             {
