db9527ce3db6d028c4b15ca828f99a313165643f
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-12-17 21:27:01 +0100

    Resolve increment operator

--- Inkrement/StrukturaDatum.cs
+++ Inkrement/StrukturaDatum.cs
@@ -36,7 +36,11 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
         }
 
         // TODO:010 Definirati operator ++ koji će objekt tipa StrukturaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
-
+        public static StrukturaDatum operator ++(StrukturaDatum datum)
+        {
+            Datum.UvećajDan(ref datum.dan,ref datum.mjesec, ref datum.godina);
+            return datum;
+        }
 
     }
 }
