610bb49715254bc51cf84f5bde437c1909619449
Ivan Posavac <ivposava@vsite.hr>
2020-12-17 21:10:22 +0100

    Resolve operatori plus minus

--- OperatoriPlusMinus/OperatoriPlusMinus.cs
+++ OperatoriPlusMinus/OperatoriPlusMinus.cs
@@ -36,13 +36,16 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
         // TODO:000 implementirati binarni operator + (za zbrajanje dva kompleksna broja), pokrenuti program i provjeriti ispis.
         public static KompleksniBroj operator +(KompleksniBroj br1, KompleksniBroj br2)
         {
-            throw new NotImplementedException();
+            return new KompleksniBroj(br1.RealniDio + br2.RealniDio, br1.ImaginarniDio + br2.ImaginarniDio);
         }
 
         // TODO:001 Implementirati binarni operator - (za oduzimanje dva kompleksna broja).
 
         // TODO:003 Implementirati preopterećeni unarni operator - (za promjenu predznaka).
-
+        public static KompleksniBroj operator -(KompleksniBroj kb)
+        {
+            return new KompleksniBroj(-kb.RealniDio, -kb.ImaginarniDio);
+        }
         // TODO:005 Pokrenuti testove (3 testa u grupi "TestOperatoraPlusMinus" moraju proći).
 
     }
@@ -64,8 +67,8 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
 
             //Naredba koja poziva unarni operator -
             // TODO:004 Otkomentirati donje dvije naredbe, pokrenuti program i provjeriti ispis.
-            //var negativni = -zbroj;
-            //Console.WriteLine($"-[({kb1}) + ({kb2})] = {negativni}");
+            var negativni = -zbroj;
+            Console.WriteLine($"-[({kb1}) + ({kb2})] = {negativni}");
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
