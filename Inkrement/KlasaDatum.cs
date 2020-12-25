3111a77bdece57082f2a22ab1586946f60395628
Jasmin Husić <jhsc1988@gmail.com>
2020-12-25 13:29:30 +0100

    Resolve increment operator

--- Inkrement/KlasaDatum.cs
+++ Inkrement/KlasaDatum.cs
@@ -54,8 +54,13 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
             return Godina ^ Mjesec ^ Dan;
         }
 
-        // TODO:013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
-
+        // 013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
+        public static KlasaDatum operator ++(KlasaDatum datum)
+        {
+            KlasaDatum noviDatum = new KlasaDatum(datum.godina, datum.mjesec, datum.dan);
+            Datum.UvećajDan(ref noviDatum.dan, ref noviDatum.mjesec, ref noviDatum.godina);
+            return datum;
+        }
 
     }
 }
