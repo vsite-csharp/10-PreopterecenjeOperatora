ccd6edba2696f9fd5955fc7ea1350ac55dd92d13
Andrea Vrljic <andrea31542@gmail.com>
2021-12-16 21:14:14 +0100

    Resolve Inkrement Struct and Class

--- Inkrement/KlasaDatum.cs
+++ Inkrement/KlasaDatum.cs
@@ -54,8 +54,12 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
             return Godina ^ Mjesec ^ Dan;
         }
 
-        // TODO:013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
-
+        // :013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
+        public static KlasaDatum operator ++(KlasaDatum datum)
+        {
+            (datum.godina, datum.mjesec, datum.dan) = Datum.UvećajDan(datum.Godina, datum.Mjesec, datum.Dan);
+            return datum;
+        }
 
     }
 }
