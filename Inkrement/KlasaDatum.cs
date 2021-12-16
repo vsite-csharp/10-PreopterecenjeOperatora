55ae3d87c08b8820f324c686a8adf1a6055cd8f8
robilovric <robilovric7@gmail.com>
2021-12-16 21:19:06 +0100

    Resolve incrementing of reference and value types

--- Inkrement/KlasaDatum.cs
+++ Inkrement/KlasaDatum.cs
@@ -54,8 +54,12 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
             return Godina ^ Mjesec ^ Dan;
         }
 
-        // TODO:013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
-
+        //:013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
+        public static KlasaDatum operator ++(KlasaDatum kd)
+        {
+            (int godina, int mjesec, int dan) = Datum.UvećajDan(kd.Godina, kd.Mjesec, kd.Dan);
+            return new KlasaDatum(godina, mjesec, dan);
+        }
 
     }
 }
