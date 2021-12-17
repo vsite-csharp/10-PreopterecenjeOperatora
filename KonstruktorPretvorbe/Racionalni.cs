03ddd188736d9a0407f40754e891577cdb5e1696
jandela <andjela.cirjak@gmail.com>
2021-12-17 17:04:55 +0100

    Resolve conversion constructor.

--- KonstruktorPretvorbe/Racionalni.cs
+++ KonstruktorPretvorbe/Racionalni.cs
@@ -19,7 +19,13 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
             return string.Format("{0} / {1}", Brojnik, Nazivnik);
         }
 
-        // TODO:021 dodati konstruktor pretvorbe (konverzije) koji stvara racionalni broj iz tipa double i u njemu treba samo pozvati metodu Raščlani.
+        // dodati konstruktor pretvorbe (konverzije) koji stvara racionalni broj iz tipa double i u njemu treba samo pozvati metodu Raščlani.
+        public Racionalni(double br)
+        {
+            (long brojnik, long nazivnik) = Raščlani(br);
+            Brojnik = brojnik;
+            Nazivnik = nazivnik;
+        }
 
         // Metoda vraća n-torku (tuple) koja se sastoji od dva broja.
         // Za to treba biti dostupna struktura System.ValuTuple koja dolazi s .NET 4.7 i .NET Core,
