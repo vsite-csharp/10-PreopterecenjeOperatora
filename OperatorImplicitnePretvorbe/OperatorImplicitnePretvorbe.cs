bd2a06b2301e658a53ad9b574050e25f5ec61404
Andrea Vrljic <andrea31542@gmail.com>
2021-12-20 09:21:56 +0100

    Resolved OperatorImplicitnePretvorbe

--- OperatorImplicitnePretvorbe/OperatorImplicitnePretvorbe.cs
+++ OperatorImplicitnePretvorbe/OperatorImplicitnePretvorbe.cs
@@ -3,9 +3,9 @@ using System.Diagnostics;
 
 namespace Vsite.CSharp.PreopterećenjeOperatora
 {
-    // TODO:030 U strukturu Racionalni iz prethodnog projekta dodati operator implicitne pretvorbe iz long u Racionalni.
+    // :030 U strukturu Racionalni iz prethodnog projekta dodati operator implicitne pretvorbe iz long u Racionalni.
 
-    // TODO:032 U strukturu Racionalni iz prethodnog projekta dodati operator implicitne pretvorbe iz double u Racionalni.
+    // :032 U strukturu Racionalni iz prethodnog projekta dodati operator implicitne pretvorbe iz double u Racionalni.
 
     class OperatorImplicitnePretvorbe
     {
@@ -14,17 +14,17 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
             try
             {
                 Racionalni rac = new Racionalni();
-                // TODO:031 Otkomentirati donje naredbe, staviti breakpointe u konstruktore implicitne pretvorbe, pokrenuti program i provjeriti ispis.
-                //rac = 2;
-                //Debug.Assert(rac.ToString() == "2 / 1");
+                // :031 Otkomentirati donje naredbe, staviti breakpointe u konstruktore implicitne pretvorbe, pokrenuti program i provjeriti ispis.
+                rac = 2;
+                Debug.Assert(rac.ToString() == "2 / 1");
                 Console.WriteLine(rac);
 
-                // TODO:033 Otkomentirati donje naredbe, pokrenuti program i provjeriti ispis.
-                //rac = 0.1;
-                //Debug.Assert(rac.ToString() == "1 / 10");
+                // :033 Otkomentirati donje naredbe, pokrenuti program i provjeriti ispis.
+                rac = 0.1;
+                Debug.Assert(rac.ToString() == "1 / 10");
                 Console.WriteLine(rac);
 
-                //TODO:034 Pokrenuti i provjeriti testove (2 testa iz grupe "TestImplicitnePretvorbe" moraju proći).
+                //:034 Pokrenuti i provjeriti testove (2 testa iz grupe "TestImplicitnePretvorbe" moraju proći).
             }
             catch (System.Exception e)
             {
