73a4bd3d6cbefde7a946aa2d397357252c79cba0
Jakov Baleta <jakov.lux@hotmail.com>
2022-12-22 20:01:10 +0100

    Resolve increment operator overloading

--- Inkrement/KlasaDatum.cs
+++ Inkrement/KlasaDatum.cs
@@ -54,8 +54,12 @@ namespace Vsite.CSharp.PreopterećenjeOperatora
             return Godina ^ Mjesec ^ Dan;
         }
 
-        // TODO:013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
-
+        // 013 definirati operator ++ koji će objekt tipa KlasaDatum uvećati za jedan dan, pozivom statičke metode Datum.UvećajDan.
+        public static KlasaDatum operator++(KlasaDatum datum)
+        {
+            (int godina,int mjesec,int dan)= (Datum.UvećajDan(datum.godina,datum.mjesec,datum.dan));
+            return new KlasaDatum(godina,mjesec,dan);
+        }
 
     }
 }
