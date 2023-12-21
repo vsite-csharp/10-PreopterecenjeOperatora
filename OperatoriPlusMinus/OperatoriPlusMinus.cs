861a237d921902306274ac0999987803ac502f99
Iva Brletić <ivbrleti@vsite.hr>
2023-12-21 21:16:55 +0100

    overload operators for complex number

--- OperatoriPlusMinus/OperatoriPlusMinus.cs
+++ OperatoriPlusMinus/OperatoriPlusMinus.cs
@@ -44,14 +44,17 @@
         // TODO:000 implementirati binarni operator + (za zbrajanje dva kompleksna broja), pokrenuti program i provjeriti ispis.
         public static KompleksniBroj operator +(KompleksniBroj br1, KompleksniBroj br2)
         {
-            throw new NotImplementedException();
+            return new KompleksniBroj(br1.RealniDio + br2.RealniDio, br1.ImaginarniDio + br2.ImaginarniDio);
         }
 
         // TODO:001 Implementirati binarni operator - (za oduzimanje dva kompleksna broja).
 
-        // TODO:003 Implementirati preopterećeni unarni operator - (za promjenu predznaka).
-
-        // TODO:005 Pokrenuti testove (3 testa u grupi "TestOperatoraPlusMinus" moraju proći).
+        // :003 Implementirati preopterećeni unarni operator - (za promjenu predznaka).
+        public static KompleksniBroj operator -(KompleksniBroj br)
+        {
+            return new KompleksniBroj(-br.RealniDio, -br.ImaginarniDio);
+        }
+        // :005 Pokrenuti testove (3 testa u grupi "TestOperatoraPlusMinus" moraju proći).
 
     }
 
@@ -71,8 +74,8 @@
             //Console.WriteLine($"({kb2}) - ({kb1}) = {razlika}");
 
             //Naredba koja poziva unarni operator -
-            // TODO:004 Otkomentirati donje dvije naredbe, pokrenuti program i provjeriti ispis.
-            //var negativni = -zbroj;
+            // :004 Otkomentirati donje dvije naredbe, pokrenuti program i provjeriti ispis.
+            var negativni = -zbroj;
             //Console.WriteLine($"-[({kb1}) + ({kb2})] = {negativni}");
 
             Console.WriteLine("GOTOVO!!!");
