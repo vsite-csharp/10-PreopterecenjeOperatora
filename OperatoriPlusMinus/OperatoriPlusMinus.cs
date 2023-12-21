ac5afa1fbacaaa3078f73c47894b15959cc75db5
Tin Bašić <tin.basic22@gmail.com>
2023-12-21 21:17:41 +0100

    Resolve overlord operators to complex number

--- OperatoriPlusMinus/OperatoriPlusMinus.cs
+++ OperatoriPlusMinus/OperatoriPlusMinus.cs
@@ -41,17 +41,24 @@
             return $"{ImaginarniDio}i";
         }
 
-        // TODO:000 implementirati binarni operator + (za zbrajanje dva kompleksna broja), pokrenuti program i provjeriti ispis.
+        // :000 implementirati binarni operator + (za zbrajanje dva kompleksna broja), pokrenuti program i provjeriti ispis.
         public static KompleksniBroj operator +(KompleksniBroj br1, KompleksniBroj br2)
         {
-            throw new NotImplementedException();
+            
+            return new KompleksniBroj(br1.RealniDio+br2.RealniDio,br1.ImaginarniDio+br2.ImaginarniDio);
+            //throw new NotImplementedException();
         }
 
         // TODO:001 Implementirati binarni operator - (za oduzimanje dva kompleksna broja).
 
-        // TODO:003 Implementirati preopterećeni unarni operator - (za promjenu predznaka).
+        // :003 Implementirati preopterećeni unarni operator - (za promjenu predznaka).
 
-        // TODO:005 Pokrenuti testove (3 testa u grupi "TestOperatoraPlusMinus" moraju proći).
+        public static KompleksniBroj operator -(KompleksniBroj broj)
+        {
+            return new KompleksniBroj(-broj.RealniDio, -broj.ImaginarniDio);
+        }
+
+        // :005 Pokrenuti testove (3 testa u grupi "TestOperatoraPlusMinus" moraju proći).
 
     }
 
@@ -71,7 +78,7 @@
             //Console.WriteLine($"({kb2}) - ({kb1}) = {razlika}");
 
             //Naredba koja poziva unarni operator -
-            // TODO:004 Otkomentirati donje dvije naredbe, pokrenuti program i provjeriti ispis.
+            // :004 Otkomentirati donje dvije naredbe, pokrenuti program i provjeriti ispis.
             //var negativni = -zbroj;
             //Console.WriteLine($"-[({kb1}) + ({kb2})] = {negativni}");
 
