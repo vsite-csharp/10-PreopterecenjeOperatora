1427013e55e3c69036d62a808abfaee3595529a5
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-01-11 20:29:58 +0100

    - do OperatorImplicitnePretvorbe

--- KonstruktorPretvorbe/Racionalni.cs
+++ KonstruktorPretvorbe/Racionalni.cs
@@ -60,5 +60,16 @@
             }
             return SljedećiVišekratnik(ostatak, manjiBroj);
         }
+
+        public static implicit operator Racionalni(long broj)
+        {
+            return new Racionalni(broj);
+        }
+
+        public static implicit operator Racionalni(double broj)
+        {
+            return new Racionalni(broj);
+        }
+
     }
 }
